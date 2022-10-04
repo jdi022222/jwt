da54ff67f32b72ff0b37bdb2648145a79f9d00e0
jdi022222 <97517890+jdi022222@users.noreply.github.com>
2022-10-04 14:23:40 +0900

    시크릿 키 원문으로 HMAC 암호화 방식에 맞는 SecretKey 객체 생성 테스트 케이스 생성

--- src/test/java/com/ll/exam/jwt/JwtApplicationTests.java
+++ src/test/java/com/ll/exam/jwt/JwtApplicationTests.java
@@ -1,9 +1,14 @@
 package com.ll.exam.jwt;
 
+import io.jsonwebtoken.security.Keys;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.crypto.SecretKey;
+
+import java.util.Base64;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
@@ -11,11 +16,19 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 class JwtApplicationTests {
 
     @Value("${custom.jwt.secretKey}")
-    private String secretKey;
+    private String secretKeyPlain;
 
     @Test
     @DisplayName("secretKey가 존재해야 한다")
     void t1() {
+        assertThat(secretKeyPlain).isNotNull();
+    }
+
+    @Test
+    @DisplayName("secretKey 원문으로 SecretKey 객체를 만들 수 있어야 한다")
+    void t2() {
+        String keyBase64Encoded = Base64.getEncoder().encodeToString(secretKeyPlain.getBytes());
+        SecretKey secretKey = Keys.hmacShaKeyFor(keyBase64Encoded.getBytes());
         assertThat(secretKey).isNotNull();
     }
 }
