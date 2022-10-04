19099f76e58d9202ca31c2ece171c54eb5ede083
jdi022222 <97517890+jdi022222@users.noreply.github.com>
2022-10-04 14:14:39 +0900

    시크릿 키가 존재 확인하는 테스트 케이스 생성

--- src/test/java/com/ll/exam/jwt/JwtApplicationTests.java
+++ src/test/java/com/ll/exam/jwt/JwtApplicationTests.java
@@ -1,13 +1,21 @@
 package com.ll.exam.jwt;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
 class JwtApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Value("${custom.jwt.secretKey}")
+    private String secretKey;
 
+    @Test
+    @DisplayName("secretKey가 존재해야 한다")
+    void t1() {
+        assertThat(secretKey).isNotNull();
+    }
 }
